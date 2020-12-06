f0cd25226aaa0a57667c137908d71047dd0461eb
AninHuang <lifeanne19904@gmail.com>
2020-12-06 21:30:50 +0800

    Add the layout trial

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,60 @@
+import 'dart:io'; // Platform
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // SystemChrome, SystemUiOverlayStyle
+import 'app_theme.dart'; // AppTheme
+import 'navigation_home_screen.dart'; // NavigationHomeScreen
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness:
+          Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.grey,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: AppTheme.textTheme,
+        platform: TargetPlatform.iOS,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NavigationHomeScreen(),
     );
   }
+  // // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       // This is the theme of your application.
+  //       //
+  //       // Try running your application with "flutter run". You'll see the
+  //       // application has a blue toolbar. Then, without quitting the app, try
+  //       // changing the primarySwatch below to Colors.green and then invoke
+  //       // "hot reload" (press "r" in the console where you ran "flutter run",
+  //       // or simply save your changes to "hot reload" in a Flutter IDE).
+  //       // Notice that the counter didn't reset back to zero; the application
+  //       // is not restarted.
+  //       primarySwatch: Colors.blue,
+  //       // This makes the visual density adapt to the platform that you run
+  //       // the app on. For desktop platforms, the controls will be smaller and
+  //       // closer together (more dense) than on mobile platforms.
+  //       visualDensity: VisualDensity.adaptivePlatformDensity,
+  //     ),
+  //     home: MyHomePage(title: 'Flutter Demo Home Page'),
+  //   );
+  // }
 }
 
 class MyHomePage extends StatefulWidget {
